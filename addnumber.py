aaf0aec4160bb8d1f69c736208188338fa09b860
SaiK <kishore_cbit@yahoo.com>
2016-12-20 13:24:57 -0800

    Including python code to add two numbers

---
+++ addnumber.py
@@ -0,0 +1,11 @@
+# this is a python program to add two numbers
+
+num1 = 5
+num2 = 3
+
+# Adding these two numbers
+sum = num1 + num2
+
+# Display the sum
+print ('The sum of {0} and {1} is: {2}'
+.format(num1,num2,sum))
